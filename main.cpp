ac11e78082b898456a580a306b3ffc5f1480901a
a.abduxakimov <a.abduxakimov@newuu.uz>
2025-04-03 17:13:25 +0500

    v2

--- main.cpp
+++ main.cpp
@@ -12,10 +12,46 @@ struct employee {
 
 int showMenu();
 void viewEmployees();
+void addEmployee();
 
 int main() {
+    int search_choice = 0;
     cout << "Welcome to the Employee System!" << endl << endl;
-    viewEmployees();
+    int user_choice = showMenu();
+    switch (user_choice) {
+        case 1: {
+            cout << "Enter 1 to view all employees: " << endl;
+            cout << "Enter 2 to search employees by id: " << endl;
+            do {
+                cout << "Enter your choice: ";
+                cin >> search_choice;
+
+                if (search_choice == 1) {
+                    viewEmployees();
+                    return 0;
+                }
+                if (search_choice == 2) {
+                    return 0; // Unfinished part
+                }
+            } while (search_choice != 2 || search_choice != 1);
+            break;
+        }
+        case 2: {
+            // Unfinished
+            break;
+        }
+        case 3: {
+            addEmployee();
+            break;
+        }
+        case 5: {
+            cout << "Exitting program..." << endl;
+            break;
+        }
+        default:
+            cout << "Invalid choice." << endl;
+    }
+
     return 0;
 }
 
@@ -38,7 +74,7 @@ int showMenu() {
 }
 
 void viewEmployees() {
-     cout << "Trying to open the file..." << endl;
+    cout << "Trying to open the file..." << endl;
     // Open the file
     ifstream file("employee.txt");
 
@@ -47,7 +83,7 @@ void viewEmployees() {
         cout << "Error! Could not open file." << endl;
         return;
     }
-
+    cout << "File opening successful." << endl;
     string line;
     // Read each line from the file and print it
     while (getline(file, line)) {
@@ -55,4 +91,28 @@ void viewEmployees() {
     }
 
     file.close();
+}
+void addEmployee() {
+    int id;
+    string name;
+    int salary;
+    string position;
+    cout << "Enter employee id: ";
+    cin >> id;
+    cin.ignore();
+    cout << "Enter employee name: ";
+    getline(cin, name);
+    cout << "Enter employee position: ";
+    getline(cin, position);
+    cout << "Enter employee salary: ";
+    cin >> salary;
+
+    ofstream file("employee.txt", ios::app);
+    if (!file) {
+        cout << "Error! Could not open file." << endl;
+        return;
+    }
+    file << "id: " << id << ", name: \"" << name << "\", salary: " << salary << ", position: \"" << position << "\"" << endl;
+    file.close();
+    cout << "Employee successfully added." << endl;
 }
